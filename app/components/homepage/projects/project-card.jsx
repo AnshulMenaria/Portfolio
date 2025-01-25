2bebe34ee9017cb7864c9de390737604d63a2c5c
AnshulMenaria <anshulmenaria@gmail.com>
2025-01-25 08:37:28 +0530

    cards

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react';
-import { motion } from 'framer-motion';
+// @flow strict
+
+import * as React from 'react';
 
 function ProjectCard({ project }) {
-  const [expanded, setExpanded] = useState(false);
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      className="relative rounded-lg border bg-gradient-to-r from-[#0d1224] to-[#0a0d37] border-[#1b2c68a0] w-full"
-    >
+    <div className="from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-full">
       <div className="flex flex-row">
         <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
         <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
@@ -27,47 +22,55 @@ function ProjectCard({ project }) {
       </div>
       <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
         <code className="font-mono text-xs md:text-sm lg:text-base">
-          {/* Dynamic Content */}
-          <div>
-            <span className="text-pink-500">const</span>
-            <span className="text-white"> Project = </span>
+          <div className="blink">
+            <span className="mr-2 text-pink-500">const</span>
+            <span className="mr-2 text-white">Project</span>
+            <span className="mr-2 text-pink-500">=</span>
             <span className="text-gray-400">{'{'}</span>
           </div>
           <div>
-            <span className="ml-4 lg:ml-8 text-white">name:</span>
-            <span className="text-amber-300">{` '${project.name}'`}</span>
+            <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
+            <span className="text-gray-400">{`'`}</span>
+            <span className="text-amber-300">{project.name}</span>
+            <span className="text-gray-400">{`',`}</span>
           </div>
-          {expanded && (
-            <>
-              <div>
-                <span className="ml-4 lg:ml-8 text-white">tools:</span>
-                <span className="text-gray-400"> [</span>
-                {project.tools.map((tool, i) => (
-                  <span key={i} className="text-amber-300">
-                    {`${tool}${i < project.tools.length - 1 ? ', ' : ''}`}
-                  </span>
-                ))}
-                <span className="text-gray-400"> ]</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 text-white">description:</span>
-                <span className="text-cyan-400">{` '${project.description}'`}</span>
-              </div>
-            </>
-          )}
+
+          <div className="ml-4 lg:ml-8 mr-2">
+            <span className=" text-white">tools:</span>
+            <span className="text-gray-400">{` ['`}</span>
+            {
+              project.tools.map((tag, i) => (
+                <React.Fragment key={i}>
+                  <span className="text-amber-300">{tag}</span>
+                  {
+                    project.tools?.length - 1 !== i &&
+                    <span className="text-gray-400">{`', '`}</span>
+                  }
+                </React.Fragment>
+              ))
+            }
+            <span className="text-gray-400">{"],"}</span>
+          </div>
           <div>
-            <span className="text-gray-400">{'};'}</span>
+            <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
+            <span className="text-orange-400">{project.role}</span>
+            <span className="text-gray-400">,</span>
           </div>
+          <div>
+            <span className="ml-4 lg:ml-8 mr-2 text-white">associated with:</span>
+            <span className="text-purple-400">{project.associate}</span>
+            <span className="text-gray-400">,</span>
+          </div>
+          <div className="ml-4 lg:ml-8 mr-2">
+            <span className="text-white">Description:</span>
+            <span className="text-cyan-400">{' ' + project.description}</span>
+            <span className="text-gray-400">,</span>
+          </div>
+          <div><span className="text-gray-400">{`};`}</span></div>
         </code>
       </div>
-      <button
-        onClick={() => setExpanded(!expanded)}
-        className="mt-4 text-center text-blue-500 hover:underline"
-      >
-        {expanded ? 'Show Less' : 'Show More'}
-      </button>
-    </motion.div>
+    </div>
   );
-}
+};
 
 export default ProjectCard;
