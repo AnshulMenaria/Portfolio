81532843611fda8f6d27748db8ded727a46a0735
AnshulMenaria <anshulmenaria@gmail.com>
2025-04-29 11:44:40 +0530

    Image Update

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -19,7 +19,7 @@ function AboutSection() {
             {personalData.description}
           </p>
         </div>
-        <div className="flex justify-center order-1 lg:order-2">
+        <div className="flex justify-center order-2 lg:order-2">
           <Image
             src={personalData.profile}
             width={280}
