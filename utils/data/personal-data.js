df65a2078a6aab565a270dbc015ee28b291ab234
AnshulMenaria <anshulmenaria@gmail.com>
2025-04-29 11:39:55 +0530

    Image & Resume Update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "ANSUL MENARIA",
-  profile: '/anshul.png',
+  profile: '/Anshul-Profile.png',
   designation: "Software Developer",
   description: "I am Ansul Menaria, a dedicated and skilled MERN stack developer with expertise in building dynamic, user-centric web applications. With a solid foundation in technologies like React.js, Node.js, Express.js, and MongoDB, I have successfully developed impactful projects, including a Weapon & Inventory Management System for the Indian Army and a Paperless Documentation System to streamline operational workflows. Currently enhancing my skills in AWS Cloud Computing and DevOps, I am driven by a relentless curiosity to explore new technologies and solve challenging problems. With a strong commitment to innovation and a collaborative mindset, I am eager to contribute to exciting projects that make a difference. Connect with me to discuss opportunities or collaborations that align with my expertise.",
   email: 'anshulmenaria@gmail.com',
@@ -10,6 +10,6 @@ export const personalData = {
   facebook: 'https://www.facebook.com/share/15hLzDWNLe/?mibextid=wwXIfr',
   linkedIn: 'https://www.linkedin.com/in/ansul-menaria/',
   twitter: 'https://twitter.com/anshul_menaria',
-  resume: "https://drive.google.com/file/d/1zUgJGC6TfCjOQYtPuPLrQZqjLZN_ODD0/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1bE5xg9F2qHVU2Js3BVps-4zJGJNgI-CU/view?usp=sharing",
   devUsername: "said7388",
 }
