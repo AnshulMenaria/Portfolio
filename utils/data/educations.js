6335eb58d51219ffd2b9dcd227309b43ad45dc6b
AnshulMenaria <anshulmenaria@gmail.com>
2025-08-07 11:59:14 +0530

    Education Duration Update

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,21 +2,21 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Computer Application",
-    duration: "2021-2024",
+    duration: "2024",
     institution: "MohanLal Sukhadia University, Udaipur,Rajasthan ",
     grade: "6.35 CGPA",
   },
   {
     id: 2,
     title: "Senior Secondary School",
-    duration: "2019-2021",
+    duration: "2021",
     institution: "St. Thomas Senior Secondary School, Fatehnagar,Rajasthan",
     grade: "76%",
   },
   {
     id: 3,
     title: "Secondary School",
-    duration: "2009-2019",
+    duration: "2019",
     institution: "St. Thomas Senior Secondary School, Fatehnagar,Rajasthan",
     grade: "53%",
   }
